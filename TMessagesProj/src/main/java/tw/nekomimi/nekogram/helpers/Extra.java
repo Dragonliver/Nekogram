83f72c7c1d5c9f28f9c8a0018c6ac3602691592b
Dragonliver <160909889+Dragonliver@users.noreply.github.com>
2026-04-08 14:45:31 +0300

    Create Extra.java

---
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/helpers/Extra.java
@@ -0,0 +1,9 @@
+package tw.nekomimi.nekogram.helpers;
+
+public class Extra {
+    public static final String SENTRY_DSN = "";
+    public static final boolean FORCE_ANALYTICS = false;
+    public static String getHelperBot() {
+        return "nekogram_bot";
+    }
+}
