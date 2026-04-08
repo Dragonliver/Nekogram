a3b90254119014aff5f3bdb88406dcb5b845cec6
Dragonliver <160909889+Dragonliver@users.noreply.github.com>
2026-04-08 15:33:42 +0300

    Update Extra.java

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/helpers/Extra.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/helpers/Extra.java
@@ -1,9 +1,29 @@
 package tw.nekomimi.nekogram.helpers;
 
 public class Extra {
+    // Константы для аналитики и ошибок
     public static final String SENTRY_DSN = "";
     public static final boolean FORCE_ANALYTICS = false;
+    
+    // Заглушка для объекта botInfo и подобных
+    public static final String botInfo = "";
+
+    // Метод для получения системного помощника
     public static String getHelperBot() {
+        return "nekogram_bot";
+    }
+
+    // Метод для получения бота информации о пользователях
+    public static String getUserInfoBot(boolean fallback) {
+        return "nekogram_bot";
+    }
+
+    // Дополнительные методы, если код ищет их в классе Extra
+    public static String getId() {
+        return "0";
+    }
+
+    public static String getUsername() {
         return "nekogram_bot";
     }
 }
